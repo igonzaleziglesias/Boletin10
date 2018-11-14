931a076b551fc50cb00b0f42ee630da772ad474e
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2018-11-14 10:24:14 +0100

    rename

--- Boletin10_2/src/boletin10_2/Xogo.java
+++ Boletin10_2/src/boletin10_2/Xogo.java
@@ -4,57 +4,57 @@ import javax.swing.JOptionPane;
 
 public class Xogo {
 
-    private int numero, intentos;
+    private int numero, numIntentos;
     private int n = (int) (Math.random() * 49) + 1;
 
     public void xogar() {
         System.out.println(n);
         do {
-            intentos = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1, Indique el numero de intentos permitidos: "));
-        } while (intentos <= 0);
+            numIntentos = Integer.parseInt(JOptionPane.showInputDialog("Jugador 1, Indique el numero de intentos permitidos: "));
+        } while (numIntentos <= 0);
 
-        for (int i = 1; i <= intentos; i++) {
-            if (i == intentos) {
+        for (int i = 1; i <= numIntentos; i++) {
+            if (i == numIntentos) {
                 JOptionPane.showMessageDialog(null, "ULTIMA OPORTUNIDAD");
             }
-            int juego = 0;
+            int intento = 0;
 
             do {
-                juego = Integer.parseInt(JOptionPane.showInputDialog("Indique o " + i + " intento: "));
+                intento = Integer.parseInt(JOptionPane.showInputDialog("Indique o " + i + " intento: "));
 
-            } while ((juego < 1) || (juego > 50));
+            } while ((intento < 1) || (intento > 50));
 
-            if (juego > n) {
+            if (intento > n) {
 
-                if (((juego - n) >= 20) || ((n - juego) >= 20)) {
+                if (((intento - n) >= 20) || ((n - intento) >= 20)) {
                     JOptionPane.showMessageDialog(null, "El numero es menor y estas muy lejos");
-                } else if ((((juego - n) >= 10) || ((n - juego) >= 10)) && (((juego - n) < 20) || ((n - juego) < 20))) {
+                } else if ((((intento - n) >= 10) || ((n - intento) >= 10)) && (((intento - n) < 20) || ((n - intento) < 20))) {
                     JOptionPane.showMessageDialog(null, "El numero es menor y estas lejos");
-                } else if ((((juego - n) < 10) || ((n - juego) < 10)) && (((juego - n) >= 5) || ((n - juego) >= 5))) {
+                } else if ((((intento - n) < 10) || ((n - intento) < 10)) && (((intento - n) >= 5) || ((n - intento) >= 5))) {
                     JOptionPane.showMessageDialog(null, "El numero es menor y estas cerca");
-                } else if (((juego - n) <= 5) || ((n - juego) <= 5)) {
+                } else if (((intento - n) <= 5) || ((n - intento) <= 5)) {
                     JOptionPane.showMessageDialog(null, "El numero es menor y estas muy cerca");
 
                 }
 
-            } else if (juego < n) {
-                if (((juego - n) >= 20) || ((n - juego) >= 20)) {
+            } else if (intento < n) {
+                if (((intento - n) >= 20) || ((n - intento) >= 20)) {
                     JOptionPane.showMessageDialog(null, "El numero es mayor y estas muy lejos");
-                } else if ((((juego - n) >= 10) || ((n - juego) >= 10)) && (((juego - n) < 20) || ((n - juego) < 20))) {
+                } else if ((((intento - n) >= 10) || ((n - intento) >= 10)) && (((intento - n) < 20) || ((n - intento) < 20))) {
                     JOptionPane.showMessageDialog(null, "El numero es mayor y estas lejos");
-                } else if ((((juego - n) < 10) || ((n - juego) < 10)) && (((juego - n) >= 5) || ((n - juego) >= 5))) {
+                } else if ((((intento - n) < 10) || ((n - intento) < 10)) && (((intento - n) >= 5) || ((n - intento) >= 5))) {
                     JOptionPane.showMessageDialog(null, "El numero es mayor y estas cerca");
-                } else if (((juego - n) <= 5) || ((n - juego) <= 5)) {
+                } else if (((intento - n) <= 5) || ((n - intento) <= 5)) {
                     JOptionPane.showMessageDialog(null, "El numero es mayor y estas muy cerca");
 
                 }
 
-            } else if (juego == n) {
+            } else if (intento == n) {
                 JOptionPane.showMessageDialog(null, "HAS ACERTADO");
-                i = intentos;
+                i = numIntentos;
 
             }
-            if ((i == intentos) && (juego != numero)) {
+            if ((i == numIntentos) && (intento != numero)) {
                 JOptionPane.showMessageDialog(null, "HAS PERDIDO");
             }
 
